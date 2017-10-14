099767230a90f54fb804f6c31bcb5950dc2eef8c
Phani <phani.dummy@hotmail.com>
2017-10-14 19:25:17 +0530

    port commentd
    port line commented

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-var port = process.env.port || 5000;
+//var port = process.env.port || 5000;
 
 var app = express();
 var apiai = require('apiai');
